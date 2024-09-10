42973bc1f69696ad454f4e174f0e2e4fe6463dd2
Willy Hardian <hardian.will@gmail.com>
2024-09-10 13:14:28 +0700

    Project setup

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+// import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1>Rock Paper Scissors</h1>
+        </>
+    );
+}
+
+export default App;
