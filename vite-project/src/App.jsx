69326f687a10c9448a077edbcf7a39372c77351c
Willy Hardian <hardian.will@gmail.com>
2024-09-10 13:26:22 +0700

    feat: add button play

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,15 @@ import { useState } from "react";
 
 function App() {
     const [score, setScore] = useState(0);
+    function playBtn() {
+        setScore(score + 1);
+    }
     return (
         <>
             <h1>Rock Paper Scissors</h1>
             <p>Fun and Play</p>
             <p>Score: {score}</p>
+            <button onClick={playBtn}>Play</button>
         </>
     );
 }
