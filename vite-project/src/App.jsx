7ad28da51ca07e2c35e70b04ec855bd5df1aa86c
Willy Hardian <hardian.will@gmail.com>
2024-09-10 13:27:02 +0700

    feat: add player images

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,14 @@ function App() {
             <h1>Rock Paper Scissors</h1>
             <p>Fun and Play</p>
             <p>Score: {score}</p>
+            <div>
+                <div>
+                    <img src="https://placekitten.com/200/300" alt="" />
+                </div>
+                <div>
+                    <img src="https://placekitten.com/200/300" alt="" />
+                </div>
+            </div>
         </>
     );
 }
