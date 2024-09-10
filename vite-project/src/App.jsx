24529a2cbe80ecc5778994a04e6fcd2f1e0f958d
Willy Hardian <hardian.will@gmail.com>
2024-09-10 13:20:35 +0700

    feat: add score system

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 // import './App.css'
 
 function App() {
+    const [score, setScore] = useState(0);
     return (
         <>
             <h1>Rock Paper Scissors</h1>
             <p>Fun and Play</p>
+            <p>Score: {score}</p>
         </>
     );
 }
